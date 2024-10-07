4fc61188b2f872bc8b6cea0d75f98da15f75b1cd
DanielRodriguezKarczewska <danirodrikar847@gmail.com>
2024-10-07 12:29:08 +0200

    2.3

---
+++ src/casoPractico7/Ej2Bisiesto.java
@@ -0,0 +1,13 @@
+package casoPractico7;
+
+import java.util.Scanner;
+
+public class Ej2Bisiesto {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce un numero N (entero): ");
+        int n = sc.nextInt();
+        sc.close();
+
+    }
+}

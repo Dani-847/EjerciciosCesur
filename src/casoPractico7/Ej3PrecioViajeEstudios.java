6dd70a2550b7bd05fac774516e0ba29046fe95e6
DanielRodriguezKarczewska <danirodrikar847@gmail.com>
2024-10-08 13:13:11 +0200

    7.5

--- src/casoPractico7/Ej3PrecioViajeEstudios.java
+++ src/casoPractico7/Ej3PrecioViajeEstudios.java
@@ -8,6 +8,14 @@ public class Ej3PrecioViajeEstudios {
         System.out.print("Introduce el numero de alumnos: ");
         int n = sc.nextInt();
 
+        int total=n>100?65*n:n>=50&&n<=99?70*n:n>=30&&n<=49?95*n:n<30&&n>0?3000:0;
+        if (total>0)
+            System.out.println("El coste total es: "+total);
+        else
+            System.out.println("La cantidad no puede ser menor a 0 alumnos.");
+
+
+        /*
         if(n>100){
             System.out.println("El coste total es " + 65*n);
         } else if (n>=50&&n<=99){
@@ -19,5 +27,6 @@ public class Ej3PrecioViajeEstudios {
         } else {
             System.out.println("La cantidad no puede ser menor a 0 alumnos.");
         }
+         */
     }
 }

5b03b9509cbf61d109eca7fd69a7a1290788804c
DanielRodriguezKarczewska <danirodrikar847@gmail.com>
2024-10-10 13:51:40 +0200

    8.1

---
+++ src/casoPractico8/Ej2Hamburger.java
@@ -0,0 +1,38 @@
+package casoPractico8;
+
+import java.util.Scanner;
+
+public class Ej2Hamburger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Que tipo de hamburguesa? ");
+        String tipoHam = sc.nextLine();
+        System.out.println("Cuantas? ");
+        int cantidad = Integer.parseInt(sc.nextLine());
+        System.out.println("Con que quieres pagar? ");
+        String tipoPago = sc.nextLine();
+
+        int precioUD = 0;
+        do {
+            if (tipoHam.toLowerCase().equals("sencilla")) precioUD=5;
+            if (tipoHam.toLowerCase().equals("doble")) precioUD=8;
+            if (tipoHam.toLowerCase().equals("triple")) precioUD=12;
+            if (precioUD==0){
+                System.out.print("Tipo invalido introduzcalo de nuevo (sencillas, dobles, medianas): ");
+                tipoHam = sc.nextLine();
+            }
+        }while (precioUD==0);
+
+        double modPago = 0;
+        do {
+            if (tipoPago.toLowerCase().equals("tarjeta")) modPago=0.95;
+            if (tipoPago.toLowerCase().equals("efectivo")) modPago=1;
+            if (modPago==0){
+                System.out.print("Tipo invalido introduzcalo de nuevo (efectivo, tarjeta): ");
+                tipoPago = sc.nextLine();
+            }
+        }while (modPago==0);
+
+        System.out.println("El total serian: " + (precioUD*cantidad)*modPago);
+    }
+}

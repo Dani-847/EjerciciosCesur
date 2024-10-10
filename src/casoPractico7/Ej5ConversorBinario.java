95b70eecbf952288591b530394f86cb1f40ccb6e
DanielRodriguezKarczewska <danirodrikar847@gmail.com>
2024-10-10 11:54:52 +0200

    7.11

--- src/casoPractico7/Ej5ConversorBinario.java
+++ src/casoPractico7/Ej5ConversorBinario.java
@@ -8,33 +8,26 @@ public class Ej5ConversorBinario {
         System.out.print("Introduce un numero: ");
         double numero = sc.nextDouble();
 
-        double binarioAux=numero;
-        double octalAux=numero;
-        double hexAux=numero;
+        double binarioAux = numero;
+        double octalAux = numero;
+        double hexAux = numero;
         String binario = "";
         String octal = "";
         String hex = "";
 
-        while (binarioAux>0.9){
-            binario = (int) (binarioAux%2) + binario;
-            binarioAux/=2;
+        while (binarioAux > 0.9) {
+            binario = (int) (binarioAux % 2) + binario;
+            binarioAux /= 2;
         }
-        while (octalAux>0.9){
-            octal = (int) (octalAux%8) + octal;
-            octalAux/=8;
+        while (octalAux > 0.9) {
+            octal = (int) (octalAux % 8) + octal;
+            octalAux /= 8;
         }
 
-
-
-        String[] hexadecimal = {"A","B","C","D","E","F"};
-        while (hexAux>0.9){
-            if (hexAux%16<=9)
-                hex = (int) (hexAux%16) + hex;
-            else{
-                hex = hexadecimal[(((int) (hexAux%16))-10)] + hex;
-
-            }
-
+        String[] hexadecimal = {"A", "B", "C", "D", "E", "F"};
+        while (hexAux > 0.9) {
+            if (hexAux % 16 <= 9) hex = (int) (hexAux % 16) + hex;
+            else hex = hexadecimal[(((int) (hexAux % 16)) - 10)] + hex;
             hexAux = (int) hexAux / 16;
         }
 

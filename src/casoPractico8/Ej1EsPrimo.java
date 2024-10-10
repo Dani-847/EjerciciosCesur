6bca16d36920b22986808d36818895e334396386
DanielRodriguezKarczewska <danirodrikar847@gmail.com>
2024-10-10 13:23:38 +0200

    8.0

--- src/casoPractico8/Ej1EsPrimo.java
+++ src/casoPractico8/Ej1EsPrimo.java
@@ -8,21 +8,16 @@ public class Ej1EsPrimo {
         System.out.print("Introduce el numero: ");
         int n = sc.nextInt();
         sc.close();
-
         boolean es = true;
         int i = 3;
-
-        if (n<1||(n%2==0&&n!=2))
+        if (n < 1 ||(n%2==0&&n!=2))
             es=false;
-
-
         while (n>i&&es){
             System.out.println(i);
             if (n%i==0)
                 es=false;
             i+=2;
         }
-
         if (es)
             System.out.println("Es primo");
         else

4fc4ed14d1b099ee22ccde8b73745c2b0eb86d6d
DanielRodriguezKarczewska <danirodrikar847@gmail.com>
2024-10-14 10:18:47 +0200

    8.2

--- src/casoPractico7/SolucionesProfesor/Ej1.java
+++ src/casoPractico7/SolucionesProfesor/Ej1.java
@@ -1,18 +1,18 @@
-package casoPractico7.SolucionesProfesor;
-
 import java.util.Scanner;
 
 public class Ej1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Escriba un númerp para dibujar el triángulo: ");
-        int altura = sc.nextInt();
+        System.out.print("Altura del triángulo: ");
+        int altura=sc.nextInt();
         sc.close();
-        for (int fila = 1; fila <= altura; fila++) {
-            for (int i = 1; i <= (altura - fila); i++) {
+        for (int fila = 1; fila <= altura ; fila++) {
+            //Pintar espacios
+            for (int i = 1; i <= (altura-fila) ; i++) {
                 System.out.print(" ");
             }
-            for (int i = 1; i <= fila; i++) {
+            //Pintar asteriscos
+            for (int i = 1; i <= fila ; i++) {
                 System.out.print("* ");
             }
             System.out.println();
--- src/casoPractico8/Ej2Hamburger.java
+++ src/casoPractico8/Ej2Hamburger.java
@@ -14,25 +14,25 @@ public class Ej2Hamburger {
 
         int precioUD = 0;
         do {
-            if (tipoHam.toLowerCase().equals("sencilla")) precioUD=5;
-            if (tipoHam.toLowerCase().equals("doble")) precioUD=8;
-            if (tipoHam.toLowerCase().equals("triple")) precioUD=12;
-            if (precioUD==0){
+            if (tipoHam.toLowerCase().equals("sencilla")) precioUD = 5;
+            if (tipoHam.toLowerCase().equals("doble")) precioUD = 8;
+            if (tipoHam.toLowerCase().equals("triple")) precioUD = 12;
+            if (precioUD == 0) {
                 System.out.print("Tipo invalido introduzcalo de nuevo (sencillas, dobles, medianas): ");
                 tipoHam = sc.nextLine();
             }
-        }while (precioUD==0);
+        } while (precioUD == 0);
 
         double modPago = 0;
         do {
-            if (tipoPago.toLowerCase().equals("tarjeta")) modPago=1.05;
-            if (tipoPago.toLowerCase().equals("efectivo")) modPago=1;
-            if (modPago==0){
+            if (tipoPago.toLowerCase().equals("tarjeta")) modPago = 1.05;
+            if (tipoPago.toLowerCase().equals("efectivo")) modPago = 1;
+            if (modPago == 0) {
                 System.out.print("Tipo invalido introduzcalo de nuevo (efectivo, tarjeta): ");
                 tipoPago = sc.nextLine();
             }
-        }while (modPago==0);
+        } while (modPago == 0);
 
-        System.out.println("El total serian: " + (precioUD*cantidad)*modPago);
+        System.out.println("El total serian: " + (precioUD * cantidad) * modPago);
     }
 }

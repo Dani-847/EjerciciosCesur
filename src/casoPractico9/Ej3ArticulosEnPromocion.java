a0592520800700680a74cdd91779f8f3f3f552cb
DanielRodriguezKarczewska <danirodrikar847@gmail.com>
2024-10-17 13:31:47 +0200

    9.3

--- src/casoPractico9/Ej3ArticulosEnPromocion.java
+++ src/casoPractico9/Ej3ArticulosEnPromocion.java
@@ -5,6 +5,34 @@ import java.util.Scanner;
 
 public class Ej3ArticulosEnPromocion {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double sumaTotal = 0;
+        String continuar = "si";
+        do {
+            System.out.print("Introduce el precio del articulo: ");
+            double precio = Double.parseDouble(sc.nextLine());
+            if (precio>=200){
+                sumaTotal += precio*0.85;
+                System.out.println("El articulo con precio, " + precio + ", tuvo un descuento del 15%.");
+            }
+            if (precio>100&&precio<200){
+                sumaTotal += precio*0.88;
+                System.out.println("El articulo con precio, " + precio + ", tuvo un descuento del 12%.");
+            }
+            if (precio<=100){
+                sumaTotal += precio*0.9;
+                System.out.println("El articulo con precio, " + precio + ", tuvo un descuento del 10%.");
+            }
+            System.out.print("Desea anyadir algun articulo mas? ");
+            continuar=sc.nextLine();
+            while (!continuar.equalsIgnoreCase("si")&&!continuar.equalsIgnoreCase("no")){
+                System.out.print("Respuesta no valida. Desea anyadir algun articulo mas? ");
+                continuar=sc.nextLine();
+            }
+        } while (continuar.equalsIgnoreCase("si"));
+        System.out.println("El total de los artulos es: " + sumaTotal);
+
+        /*
         System.out.print("Introduce el numero de articulos: ");
         Scanner sc = new Scanner(System.in);
         int tamanyo=sc.nextInt();
@@ -34,6 +62,6 @@ public class Ej3ArticulosEnPromocion {
             }
         }
 
-        System.out.println("El total es: " + precioTotal + "€");
+        System.out.println("El total es: " + precioTotal + "€");*/
     }
 }

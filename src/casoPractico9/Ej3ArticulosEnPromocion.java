b321e316b37b231ddb85f75ed752708d2fbfc331
DanielRodriguezKarczewska <danirodrikar847@gmail.com>
2024-10-17 13:01:19 +0200

    9.3

---
+++ src/casoPractico9/Ej3ArticulosEnPromocion.java
@@ -0,0 +1,39 @@
+package casoPractico9;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Ej3ArticulosEnPromocion {
+    public static void main(String[] args) {
+        System.out.print("Introduce el numero de articulos: ");
+        Scanner sc = new Scanner(System.in);
+        int tamanyo=sc.nextInt();
+        double[] listaNumeros = new double[tamanyo];
+
+        System.out.println("Introduce el precio del "+listaNumeros.length+"º articulo.");
+
+        for (int i = 0;i < listaNumeros.length;i++){
+            System.out.print("Articulo Nº"+ (i+1) + ": ");
+            listaNumeros[i] = sc.nextInt();
+        }
+        sc.close();
+
+        double precioTotal = 0;
+        for (int i = 0;i<listaNumeros.length;i++){
+            if (listaNumeros[i]>=200){
+                precioTotal += listaNumeros[i]*1.15;
+                System.out.println("El articulo Nº"+ (i+1) + " con precio, " + listaNumeros[i] + ", tuvo un descuento del 15%.");
+            }
+            if (listaNumeros[i]>100&&listaNumeros[i]<200){
+                precioTotal += listaNumeros[i]*1.12;
+                System.out.println("El articulo Nº"+ (i+1) + " con precio, " + listaNumeros[i] + ", tuvo un descuento del 12%.");
+            }
+            if (listaNumeros[i]<=100){
+                precioTotal += listaNumeros[i]*1.1;
+                System.out.println("El articulo Nº"+ (i+1) + " con precio, " + listaNumeros[i] + ", tuvo un descuento del 10%.");
+            }
+        }
+
+        System.out.println("El total es: " + precioTotal + "€");
+    }
+}

2416d9b2aea4ecea919550fe403dc468cbdf5689
DanielRodriguezKarczewska <danirodrikar847@gmail.com>
2024-10-17 12:24:40 +0200

    9.2

--- src/casoPractico9/Ej2FabricaDiurnaNocturna.java
+++ src/casoPractico9/Ej2FabricaDiurnaNocturna.java
@@ -18,16 +18,21 @@ public class Ej2FabricaDiurnaNocturna {
             System.out.print("Turno invalido. Turno(Nocturno, Diurno)? ");
             turno = sc.nextLine();
         }
-        double tarifaTurno = turno.equals("diurno")? 10:13.5;
         System.out.print("Dia? ");
         String dia = sc.nextLine();
         while (!(dia.equalsIgnoreCase("festivo"))&&!(dia.equalsIgnoreCase("laborable"))) {
             System.out.print("Dia invalido. Dia(Festivo, Laborable)? ");
             turno = sc.nextLine();
         }
-        double tarifaDia = turno.equalsIgnoreCase("festivo")? 1.10:1;
-        if (turno.equalsIgnoreCase("nocturno")&&dia.equalsIgnoreCase("festivo"))
-            tarifaDia=1.15;
-        System.out.println("El sueldo del trabajador es: " + (horas*tarifaTurno*tarifaDia));
+
+        double tarifaFinal = turno.equals("diurno")? 10:13.5;
+        if (dia.equalsIgnoreCase("festivo")){
+            if (turno.equalsIgnoreCase("nocturno"))
+                tarifaFinal*=1.15;
+            else
+                tarifaFinal*=1.10;
+        }
+
+        System.out.println("El sueldo del trabajador es: " + (horas*tarifaFinal));
     }
 }

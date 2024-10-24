3ad468e6a99626cf74f81429d9ebe560aa6ab277
DanielRodriguezKarczewska <danirodrikar847@gmail.com>
2024-10-24 10:05:32 +0200

    10.4

--- src/casoPractico10/Ej4PapelPiedraTijeras.java
+++ src/casoPractico10/Ej4PapelPiedraTijeras.java
@@ -8,63 +8,40 @@ public class Ej4PapelPiedraTijeras {
         Scanner sc = new Scanner(System.in);
         Random r = new Random();
         int puntosJugador = 0;
-        int puntosPC= 0;
+        int puntosPC = 0;
+        int cnt = 0;
 
-        int arma = 0;
         do {
-            System.out.println("Elije tu arma (-1 para terminar el juego)");
+            System.out.println("Elije tu arma");
             System.out.println("0.- Piedra");
             System.out.println("1.- Papel");
             System.out.println("2.- Tijeras");
-            arma = sc.nextInt();
+            int arma = sc.nextInt();
             int armaOrdenador = r.nextInt(2);
             System.out.println("Arma jugador: " + arma + " - Arma ordenador: " + armaOrdenador);
-            int condVict = -1;
-
             if (arma == armaOrdenador)
                 System.out.println("Empate");
-
-            if (arma==0&&armaOrdenador==1){
-                System.out.print("Gana el ordenador. ");
-                condVict=0;
-                puntosPC++;
-            }
-            if (arma==0&&armaOrdenador==2){
-                System.out.print("Gana el jugador. ");
-                condVict=2;
+            else if ((arma == 0 && armaOrdenador == 2) || (arma == 1 && armaOrdenador == 0) || (arma == 2 && armaOrdenador == 1)) {
+                System.out.println("Gana el jugador.");
                 puntosJugador++;
-            }
-            if (arma==1&&armaOrdenador==0){
-                System.out.print("Gana el jugador. ");
-                condVict=0;
-                puntosJugador++;
-            }
-            if (arma==1&&armaOrdenador==2){
-                System.out.print("Gana el ordenador. ");
-                condVict=1;
+                if (arma == 0) System.out.println("Piedra aplasta Tijeras");
+                if (arma == 1) System.out.println("Papel envuelve a Piedra");
+                if (arma == 2) System.out.println("Tijera corta Papel");
+            } else {
+                System.out.println("Gana el ordenador.");
                 puntosPC++;
-            }
-            if (arma==2&&armaOrdenador==0){
-                System.out.print("Gana el ordenador. ");
-                condVict=2;
-                puntosPC++;
-            }
-            if (arma==2&&armaOrdenador==1){
-                System.out.print("Gana el jugador. ");
-                condVict=1;
-                puntosJugador++;
-            }
+                if (armaOrdenador == 0) System.out.println("Piedra aplasta Tijeras");
+                if (armaOrdenador == 1) System.out.println("Papel envuelve a Piedra");
+                if (armaOrdenador == 2) System.out.println("Tijera corta Papel");
 
-            if (condVict == 0)
-                System.out.println("Papel envuelve a Piedra");
-            if (condVict == 1)
-                System.out.println("Tijera corta Papel");
-            if (condVict == 2)
-                System.out.println("Piedra aplasta Tijeras");
-            System.out.println("Marcador");
-            System.out.println("========");
-            System.out.println("Jugador: "+puntosJugador+" - Ordenador: "+puntosPC);
-        }while(arma!=-1);
 
+                System.out.println("Marcador");
+                System.out.println("========");
+                System.out.println("Jugador: " + puntosJugador + " - Ordenador: " + puntosPC);
+            }
+            System.out.print("Desea continuar (si:1 no:0)? ");
+            cnt=sc.nextInt();
+            System.out.println("=============================");
+        }while (cnt != -1);
     }
 }

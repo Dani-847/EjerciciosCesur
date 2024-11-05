1d839510e8db07662a514105017e4c6b66f33f7a
DanielRodriguezKarczewska <danirodrikar847@gmail.com>
2024-11-05 12:59:41 +0100

    POO 4.0

---
+++ src/pooCasoPractico4/RectanguloTest.java
@@ -0,0 +1,16 @@
+package pooCasoPractico4;
+
+import pooCasoPractico3.Libro;
+
+public class RectanguloTest {
+    public static void main(String[] args) {
+        Rectangulo r1 = new Rectangulo(2,4);
+        Rectangulo r2 = new Rectangulo(2.5,12.5);
+
+
+        System.out.println("Area 1 "+r1.calcularArea());
+        System.out.println("Perimetro 1 "+r1.calcularPerimetro());
+        System.out.println("Area 2 "+r2.calcularArea());
+        System.out.println("Perimetro 2 "+r2.calcularPerimetro());
+    }
+}

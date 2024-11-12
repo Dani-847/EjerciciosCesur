944e698daaab5083c9f2332f1d46e5620d84a3e0
DanielRodriguezKarczewska <danirodrikar847@gmail.com>
2024-11-12 13:18:29 +0100

    POO UD4CP1

--- src/pooUD4CasoPractico1/Main.java
+++ src/pooUD4CasoPractico1/Main.java
@@ -3,9 +3,14 @@ package pooUD4CasoPractico1;
 public class Main {
     public static void main(String[] args) {
 
-        Humano humano1 = new Humano("Juan", (byte) 3);
-        Humano humano2 = new Humano("Pedro", (byte) 5);
+        SerVivo x = new SerVivo((byte) 3);
+        SerVivo y = new SerVivo((byte) 5);
 
-        System.out.println();
+        System.out.println(y.mayor(x));
+
+        Humano humano1 = new Humano("Homero", (byte) 34);
+        Humano humano2 = new Humano("Bart", (byte) 9);
+
+        System.out.println(humano2.mayor(humano1));
     }
 }

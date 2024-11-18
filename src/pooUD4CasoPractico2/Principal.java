34d5351da14c9998f8b33a60c175dce3e4fae566
DanielRodriguezKarczewska <danirodrikar847@gmail.com>
2024-11-18 10:56:32 +0100

    POO UD4CP2

---
+++ src/pooUD4CasoPractico2/Principal.java
@@ -0,0 +1,15 @@
+package pooUD4CasoPractico2;
+
+public class Principal {
+    public static void main(String[] args) {
+        Persona p1 = new Persona("Juan", "Perez", 123456789);
+        Persona p2 = new Persona("Ana", "Lopez", 987654321);
+        Cuenta c1 = new Cuenta("123456", 1000, p1);
+        Cuenta c2 = new Cuenta("654321", 2000, p2);
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(p2);
+        System.out.println(p1);
+        c1.transaccion(782136, "ingresar");
+    }
+}

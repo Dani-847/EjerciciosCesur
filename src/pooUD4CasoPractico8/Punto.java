9a52e3fd311396b7e972831ff7a0d20f62a6e431
DanielRodriguezKarczewska <danirodrikar847@gmail.com>
2024-11-26 13:31:15 +0100

    POO UD4CP7 y 8

---
+++ src/pooUD4CasoPractico8/Punto.java
@@ -0,0 +1,28 @@
+package pooUD4CasoPractico8;
+
+public class Punto {
+    private double x;
+    private double y;
+    public Punto(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+    public double distancia(Punto p) {
+        return Math.sqrt(Math.pow(p.getX() - x, 2) + Math.pow(p.getY() - y, 2));
+    }
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
---
+++ src/pooUD4CasoPractico8/Recta.java
@@ -0,0 +1,35 @@
+package pooUD4CasoPractico8;
+
+public class Recta extends Vector{
+    public Recta (Punto p, Punto q) {
+        super(p, q);
+    }
+    public Recta (Vector v, Punto p) {
+        super(v, p);
+    }
+    public double distancia(Punto p) {
+        return Math.abs(this.getX() * p.getY() - this.getY() * p.getX()) / this.modulo();
+    }
+    public Punto interseccion(Recta r) {
+        double x = (this.getY() * r.getX() - this.getX() * r.getY()) / (this.getX() * r.componenteY() - this.getY() * r.componenteX());
+        double y = (this.getX() * r.componenteY() - this.getY() * r.getX()) / (this.getX() * r.componenteY() - this.getY() * r.componenteX());
+        return new Punto(x, y);
+    }
+    public Recta paralela(Punto p) {
+        return new Recta(this, p);
+    }
+    public boolean paralelas(Recta r) {
+        return this.getX() * r.componenteY() == this.getY() * r.componenteX();
+    }
+    public boolean pasa(Punto p) {
+        return this.getX() * p.getY() == this.getY() * p.getX();
+    }
+    public Recta perpendicular(Punto p) {
+        return new Recta(new Vector(-this.getY(), this.getX()), p);
+    }
+    @Override
+    public String toString() {
+        return "Recta: " + super.toString();
+    }
+
+}

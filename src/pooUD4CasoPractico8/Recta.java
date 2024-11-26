3db70d5944e77064dcf3fafe50a52196b4d80951
DanielRodriguezKarczewska <danirodrikar847@gmail.com>
2024-11-26 14:15:13 +0100

    POO UD4CP8

--- src/pooUD4CasoPractico8/Recta.java
+++ src/pooUD4CasoPractico8/Recta.java
@@ -17,12 +17,14 @@ public class Recta extends Vector{
         return p.distancia(interseccion);
     }
     public Punto interseccion(Recta r) {
-        //
-        double d;
-        double d1;
-        double d2;
-        double abscisa;
-        double ordenada;
+        //R(V(vx,vy),P(px,py)) y R(V(ux,uy),P(qx,qy))
+        //this.getX() this.getY() punto     y el otro es  v.getX() v.getY() recta
+        Vector u = new Vector(this.ortogonal());
+        double d = u.getY() * this.getY() - this.getY() * u.getX();
+        double d1 = this.getY() * this.punto.getY() - this.getY() * this.punto.getX();
+        double d2 = u.getX() * r.punto.getY() - u.getY() * r.punto.getX();
+        double abscisa = (d1 * u.getX() - d2 * this.getX()) / d;
+        double ordenada = (d1 * u.getY() - d2 * this.getY()) / d;
         return new Punto(abscisa, ordenada);
     }
     public Recta paralela(Punto p) {

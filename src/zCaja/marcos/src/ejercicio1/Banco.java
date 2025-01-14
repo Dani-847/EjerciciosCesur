405a4be6503c08fc2104cb1e0f36f2ea74960f5c
DanielRodriguezKarczewska <danirodrikar847@gmail.com>
2025-01-14 10:48:49 +0100

    POO UD6CP4 3

--- src/zCaja/marcos/src/ejercicio1/Banco.java
+++ src/zCaja/marcos/src/ejercicio1/Banco.java
@@ -9,7 +9,15 @@ public class Banco {
     }
 
     public void agregarCuenta(CuentaBancaria cuenta) {
-
+        boolean agregado = false;
+        int i = 0;
+        while (i< cuentas.length && !agregado) {
+            if (cuentas[i] == null) {
+                cuentas[i] = cuenta;
+                agregado = true;
+            }
+            i++;
+        }
     }
 
     public double totalDinero(){

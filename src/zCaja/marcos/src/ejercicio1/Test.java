9c75648735f3f14258871dd0707ddb41219b503e
DanielRodriguezKarczewska <danirodrikar847@gmail.com>
2025-01-14 10:53:09 +0100

    POO UD6CP4 3

--- src/zCaja/marcos/src/ejercicio1/Test.java
+++ src/zCaja/marcos/src/ejercicio1/Test.java
@@ -1,13 +1,22 @@
 package zCaja.marcos.src.ejercicio1;
 
     public class Test {
-        public static <Banco> void main(String[] args) {
-            Banco banco = (Banco) new CuentaBancaria[3];
+        public static void main(String[] args) {
+            Banco banco = new Banco(3);
 
-            CuentaBancaria.cuenta(new CuentaBancaria("Juan Pérez", 5000.0, TipoCuenta.AHORRO));
-            CuentaBancaria.cuenta(new CuentaBancaria("Ana López", 3000.0, TipoCuenta.CORRIENTE));
-            CuentaBancaria.cuenta(new CuentaBancaria("Carlos García", 10000.0, TipoCuenta.EMPRESARIAL));
-            System.out.println();
+            CuentaBancaria cuenta1 = new CuentaBancaria("Juan Pérez", 5000.0, 300, TipoCuenta.AHORRO);
+            CuentaBancaria cuenta2 = new CuentaBancaria("Ana López", 3000.0, 32, TipoCuenta.CORRIENTE);
+            CuentaBancaria cuenta3 = new CuentaBancaria("Carlos García", 10000.0, 12, TipoCuenta.EMPRESARIAL);
+            banco.agregarCuenta(cuenta1);
+            banco.agregarCuenta(cuenta2);
+            banco.agregarCuenta(cuenta3);
+            cuenta1.realizarTransaccion(1000, 1);
+            cuenta2.realizarTransaccion(500, 2);
+            cuenta3.realizarTransaccion(2000, 1);
+            cuenta1.calcularInteres();
+            cuenta2.calcularInteres();
+            cuenta3.calcularInteres();
+            System.out.println("Total dinero en el banco: " + banco.totalDinero());
         }
     }
 

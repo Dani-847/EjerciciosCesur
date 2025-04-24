3eaca155476805946fc64a5b82a28132b536ee33
DanielRodriguezKarczewska <danirodrikar847@gmail.com>
2025-04-24 11:55:10 +0200

    Examen

---
+++ src/Examen/LectorDeEmpleados.java
@@ -0,0 +1,10 @@
+package Examen;
+
+import javax.swing.*;
+
+public class LectorDeEmpleados extends JFrame{
+    private JPanel panel1;
+    private JButton btnCargarCSV;
+    private JComboBox comboBox1;
+    private JTextPane tpArchivo;
+}
---
+++ src/Examen/Main.java
@@ -0,0 +1,12 @@
+package Examen;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            LectorDeEmpleados lectorDeEmpleados = new LectorDeEmpleados();
+            lectorDeEmpleados.setVisible(true);
+        });
+    }
+}
